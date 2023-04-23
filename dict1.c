f017fb91289d52710a0bd87a11867b9ac5b6fc2f
Christian Morabito <christian_morabito@hotmail.com>
2023-04-23 13:57:28 +1000

    Minor update on 'record struct' algorithm.

--- dict1.c
+++ dict1.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #define MAX_FIELD 128
 #define MAX_RECORD 512
-
+#define FIELD_N 19
 
 typedef struct{
     int fPathId;
@@ -11,6 +11,19 @@ typedef struct{
     char* clueSa;
     char* assetType;
     double deltaZ;
+    int mccId;
+    int mccIdInt;
+    double rLMax;
+    double rLMin;
+    char* segSide;
+    int statusId;
+    int streetId;
+    int streetGroup;
+    double startLat;
+    double startLon;
+    double endLat;
+    double endLon;
+
 }Record;
 typedef struct Node{
     Record* data;
@@ -18,27 +31,41 @@ typedef struct Node{
 
 }Node;
 
+char* createString(int right, int left, char* tmpField){
+    char* string = calloc(strlen(tmpField), sizeof(char));
+    strncpy(string, tmpField, (right - left));
+    return string;
+
+}
+
 Record* importRec(char* stringRec){
     Record* structRec = malloc(sizeof(Record));
-    int right, left=0, i=0, comma=0;
+    int right, left=0;
+    int i=0, comma=1;
     char tmpField[MAX_FIELD];
     memset(tmpField, '\0', MAX_FIELD);
-    for (right = 0; *stringRec != '\0'; stringRec++, right++){
+    for (right=0; *stringRec != '\0'; right++, stringRec++){
         if (*stringRec == ','){
 
             switch (comma){
-                case 0:
+                case 1:
+                    structRec->fPathId = atoi(tmpField);
+                    break;
+                case 2:
+                    structRec->address = createString(right, left, tmpField);
+                    break;
+                case 3:
+                    structRec->clueSa = createString(right, left, tmpField);
+                    break;
 
+                default:
+                    printf("Error");
             }
-
-
-//            char* address = calloc(right, sizeof(char));
-//            strncpy(address, tmpField + left, right - left);
-//            structRec->address = address;
-//            memset(tmpField, '\0', right-left);
-//            left = right;
-//            i = 0; // reset tmpField iterator
-//            continue;
+            memset(tmpField, '\0', (right-left)-1);
+            left = right;
+            i = 0; // reset tmpField iterator
+            comma++;
+            continue;
         }
         tmpField[i] = *stringRec;
         i++;
@@ -65,9 +92,9 @@ void insert(Node** head, char* strRec){
 int main(){
     Node* head = NULL;
 //    FILE* fPtr = fopen("prac.csv", "r");
-    char buffer[200] = {"27665,Palmerston Street between Rathdowne Street and Drummond Street,Carlton,Road Footway,3.21"};
+    char buffer[MAX_RECORD] = {"453452433,bob,carlton Road Footway,3.21"};
+//27,Palmerston Street between Rathdowne Street and Drummond Street,Carlton
 //    fgets(buffer,MAX_RECORD,fPtr);
     insert(&head, buffer);
 
 }
-// TEST
